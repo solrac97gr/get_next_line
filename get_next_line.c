82893738d107bf6ad1f1915272f1e474af8d1ea0
Carlos Alberto Garcia Rosales <carloga2@c3r3s3.42malaga.com>
2025-05-23 18:43:50 +0200

    first try

--- get_next_line.c
+++ get_next_line.c
@@ -6,27 +6,40 @@
 /*   By: carloga2 <carloga2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/21 15:35:08 by carloga2          #+#    #+#             */
-/*   Updated: 2025/05/21 17:16:12 by carloga2         ###   ########.fr       */
+/*   Updated: 2025/05/23 18:43:41 by carloga2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include "stdio.h"
+#include <stdlib.h>
+#include <string.h>
 
+/**
+* @brief Reads a line from a file descriptor.
+* @param fd The file descriptor to read from.
+* @return A pointer to the line read, or NULL 
+* if an error occurs or EOF is reached.
+*/
 char	*get_next_line(int fd)
 {
-	int		len;
-	char	*buff[BUFFER_SIZE];
+	char	*buffer;
+	int		bytes_read;
+	char	*line;
 
-
-	printf("GET NEXT LINE START")
-	len = 0;
-	len = read(fd, buff, len);
-	while (*buff)
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	buffer = malloc(BUFFER_SIZE + 1);
+	if (!buffer)
+		return (NULL);
+	bytes_read = read(fd, buffer, BUFFER_SIZE);
+	if (bytes_read <= 0)
 	{
-		len--;
-		return (char *)buff[len-1];
+		free(buffer);
+		return (NULL);
 	}
-	printf("GET NEXT LINE END")
-	return (NULL);
+	buffer[bytes_read] = '\0';
+	line = strdup(buffer);
+	free(buffer);
+	return (line);
 }
